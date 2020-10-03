c605e77cb00ae12ac9619e9b9ddfac28fb65b385
j.u.p.iter <pn79122821224@gmail.com>
2020-10-03 17:20:45 +0500

    test(watch mode): exclude json files from watching

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const originalConfig = require('@j.u.p.iter/jupiter-scripts/dist/lib/config/jest.config');
+
+module.exports = {
+  ...originalConfig,
+  collectCoverageFrom: [
+    ...originalConfig.collectCoverageFrom,
+    "!**/*.json",
+  ]
+};
